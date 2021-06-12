c836af51839a156d51a685f27a1e2940e0419d31
Mayank Gupta <mgupta02@in.ibm.com>
2021-06-12 21:45:32 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # weather-service
+# cb-credential-service
+1. clone the git repository from following url
+```
+  $ git clone git@github.com:mayankmike86/weather-service.git
+```
+2. Move to weather-service folder
+```
+  $ cd weather-service
+```
+3. Run following Command
+```
+  $ npm install
+```
+
+5. Run following command to start server
+```
+	node app
+```
--- app.js
+++ app.js
@@ -7,10 +7,11 @@ const port = 8000;
 var vault = null;
 const baseUrl = '/weather-forecast-service/api/v1.0';
 
-var accountRoutes = require('./src/app/v1/routes/weather-routes');
+var weatherRoutes = require('./src/app/v1/routes/weather-routes');
 
 
-app.use(baseUrl, accountRoutes);
+app.use(baseUrl, weatherRoutes);
 app.listen(port, () => {  
     console.log('Cloud Brokerage Credential Service is live on ' + port);
 });
+
